ab001e678441aedd989521a209199e2752a28635
Yuan Wu <wu.yuanf@gmail.com>
2023-03-16 14:56:26 -0400

    commit pueduo code

---
+++ script.js
@@ -0,0 +1,26 @@
+// SPACE BATTLE OOP
+
+// < ------------ ALIENS (USER) ------------ >
+// build 6 alien ship, start w 1 and make sure it's functionality
+// aliens's weakness: attack 1 at a time
+// depoly another ship after seeing the outcome of a battle
+
+
+// < ------------ USS (ME) ------------ >
+// attack first
+// only attack in order
+// if alien ship destroyed, option to make a hasty retreat
+
+
+// < ------------ the game ------------ >
+// < ------------ attack phrase ------------ >
+// USS attack alien ship #1
+// if alien ship survives, it attacks USS
+// if USS shp survives, USS attacks again
+// if alien survives, alien attacks again and so on
+
+// < ------------ destructive phrase ------------ >
+// if USS destroy alien, USS has the option to attack alien ship #2 or retreat
+// if USS retreat, GAME OVER
+// USS wins the game if all alien ships are destroyed
+// USS lose if USS ship
