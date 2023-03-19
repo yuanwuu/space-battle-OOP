9438d14b93afac1711688491bcf6663ed6c9bd27
Yuan Wu <wu.yuanf@gmail.com>
2023-03-19 17:58:20 -0400

    reorganized code

--- script.js
+++ script.js
@@ -39,8 +39,9 @@ firepower: the amount of damage done to the hull of the target with a successful
 accuracy: the chance between 0 and 1 that the ship will hit its target
 */
 
-//window prompt to start <-- done!
 
+
+// < --------------- Window Prompt ------------------- > 
 // let guest = prompt (`Hey, how are you? \n wanna play a game? \n and your name is?`)
 // if (guest === null || guest === '') {
 //     txt = 'hi, no name';
@@ -55,19 +56,37 @@ accuracy: the chance between 0 and 1 that the ship will hit its target
 // } else {
 //     alert (`open your console and start playing!`)
 // }
-// <-- window prompt Done!
+// < --------------- Window Prompt ------------------- > 
 
 
 
+
+// < --------------- fns to generate random # in range ------------------- > 
+/* 
 const alienAccuracy = (min, max) => { // alien ship accuracy num
     return Number(Math.random().toFixed(1));
 }
-
 
 function randomNum(min, max) { // Random num b/w 3 to 6
     return Math.floor(Math.random() * (max - min) + min);
 }
 
+const aHull = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);  
+}
+
+const aFire = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);  
+}
+
+const aAcc = (min, max) => {
+    return Number(Math.random().toFixed(1));
+}
+
+
+const alien = new Ship (randomNum(3,6),randomNum(2,4),alienAccuracy(.6,.8))
+const alien = new Alienships ((3,6), (2,4), (.6,.8))
+*/
 
 class Ship {
     constructor(name,hull,firepower,accuracy){
@@ -92,87 +111,65 @@ class Ship {
     }
 }
 
-// const aHull = (min, max) => {
-//     return Math.floor(Math.random() * (max - min) + min);  
-// }
-
-
-// const aFire = (min, max) => {
-//     return Math.floor(Math.random() * (max - min) + min);  
-// }
-
-// const aAcc = (min, max) => {
-//     return Number(Math.random().toFixed(1));
-// }
-
-
 class Alienships {
     constructor(name){
         this.name = name
-        this.ahull = Math.floor(Math.random() * 4) + 3
-        this.aFire = Math.floor(Math.random() * 3) + 2
-        this.aAcc = (Math.floor(Math.random() * 3) + 6) / 10
+        this.aHull = Math.floor(Math.random() * 4) + 3 // Random num b/w 3 to 6
+        this.aFire = Math.floor(Math.random() * 3) + 2 // Random num b/w 2 to 4
+        this.aAcc = (Math.floor(Math.random() * 3) + 6) / 10 // Random num from .6 to .8
     }
 }
 
 
-
-// param: hull,firepower,accuracy
-// const alien = new Ship (randomNum(3,6),randomNum(2,4),alienAccuracy(.6,.8))
-// const alien = new Alienships ((3,6), (2,4), (.6,.8))
-
-
-
 let alienTeam = [];
-alienTeam.push((al1 = new Ship('AL #1')));
-alienTeam.push((al2 = new Ship('AL #2')));
-alienTeam.push((al3 = new Ship('AL #3')));
-alienTeam.push((al4 = new Ship('AL #4')));
-alienTeam.push((al5 = new Ship('AL #5')));
-alienTeam.push((al6 = new Ship('AL #6')));
-alienTeam.push((al7 = new Ship('AL #7')));
+alienTeam.push((al1 = new Ship('Alien #1')));
+alienTeam.push((al2 = new Ship('Alien #2')));
+alienTeam.push((al3 = new Ship('Alien #3')));
+alienTeam.push((al4 = new Ship('Alien #4')));
+alienTeam.push((al5 = new Ship('Alien #5')));
+alienTeam.push((al6 = new Ship('Alien #6')));
+alienTeam.push((al7 = new Ship('Alien #7')));
 // console.log(alienTeam)
 
 const alien = new Alienships(alienTeam[0])
 const uss = new Ship ('USS',20,5,.7)
-console.log (alien)
-console.log (uss)
+// console.log (alien)
+// console.log (uss)
 
 
 
+// < ---------------Alien attacks USS------------------- > 
 
-
-
-// // alien #1 attacks uss
-// if (alienAccuracy(.6,.8) > uss.accuracy){
-//     let alienNewHp = (alien.hull - uss.hull)* -1
-//     let alienNewFp = (alien.firepower - uss.firepower)* -1
-//     console.log(`Alien: USS have been hit! \n 
-//     uss current hull: ${alienNewHp} \n 
-//     uss current firepower: ${alienNewFp}`)
+if (alien.aAcc > uss.accuracy){
+    let alienNewHp = (alien.aHull - uss.hull)* -1
+    let alienNewFp = (alien.aFire - uss.firepower)* -1
+    console.log(`Alien: USS have been hit! \n 
+    uss current hull: ${alienNewHp} \n 
+    uss current firepower: ${alienNewFp}`)
     
-    
-// } else {
-//     console.log(`Alien #2 prepare! \n Alien Hull: ${alien.hull} \n Alien Firepower: ${alien.firepower}`)
-//     // console.log(alien)
-// }
+} else {
+    console.log(`Alien #2 prepare! \n 
+    Alien Hull: ${alien.aHull} \n 
+    Alien Firepower: ${alien.aFire}`)
+    // console.log(alien)
+}
 
-// // USS attacks Alien
-// if (uss.accuracy > alienAccuracy(.6,.8)){
-//     let ussNewHp = (uss.hull - alien.hull)
-//     let ussNewFp = (uss.firepower - alien.firepower)
-//     console.log(`USS: Alien#1 have been hit! \n 
-//     alien current hull: ${ussNewHp} \n 
-//     alien current firepower: ${ussNewFp}`)
-   
-   
-//     // uss.hull - alien.hull
-//     // uss.firepower - alien.firepower
-//     // console.log(`USS: hit made! \n USS Hull: ${uss.hull} \n USS Firepower: ${uss.firepower}`)
-// } else {
-//     console.log(`USS Retreat! \n USS Hull: ${uss.hull} \n USS Firepower: ${uss.firepower}`)
-//     // console.log(uss)
-// }
+
+// < ---------------USS attacks Alien------------------- > 
+
+if (uss.accuracy > alien.aAcc){
+    let ussNewHp = (uss.hull - alien.aHull)
+    let ussNewFp = (uss.firepower - alien.aFire)
+    console.log(`USS: Alien#1 have been hit! \n 
+    alien current hull: ${ussNewHp} \n 
+    alien current firepower: ${ussNewFp}`)
+    // uss.hull - alien.hull
+    // uss.firepower - alien.firepower
+    // console.log(`USS: hit made! \n USS Hull: ${uss.hull} \n USS Firepower: ${uss.firepower}`)
+} else {
+    console.log(`USS Retreat! \n USS Hull: ${uss.hull} \n USS Firepower: ${uss.firepower}`)
+    // console.log(uss)
+}
 
 
 
@@ -188,7 +185,7 @@ console.log (uss)
 // }
 
 
-// if (alien.hull <= 0 ){
+// if (alien.aHull <= 0 ){
 //     window.alert(`USS saved the universe from alien's attack`)
 // } else {
 //     window.alert(`USS, too young too naive. Dont run into me in your next life`)
